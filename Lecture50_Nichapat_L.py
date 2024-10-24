02f266284af2fc8b66ecd68cf1abebac8b8c2e4d
AntNichapat <146622315+Nichapat05@users.noreply.github.com>
2024-10-24 13:30:45 +0700

    Lec50

---
+++ Lecture50_Nichapat_L.py
@@ -0,0 +1,15 @@
+def addNumber(x, y):
+    print(x + y)
+def subNumber(x, y):
+    print(x - y)
+def mulNumber(x, y):
+    print(x * y)
+def divNumber(x, y):
+    print(x / y)
+x = int(input("Input first number: "))
+y = int(input("Input second number: "))
+
+addNumber(x, y)
+subNumber(x, y)
+mulNumber(x, y)
+divNumber(x, y)
