ad392735ddeb572887ab0d6719b5f4d19077a860
AntNichapat <146622315+Nichapat05@users.noreply.github.com>
2024-10-27 09:28:30 +0700

    Ex4

--- Exercise4_Nichapat_L.py
+++ Exercise4_Nichapat_L.py
@@ -1,2 +1,9 @@
-y = "5"
-print(y+5)
+FE = input("Enter Foundation English Score : ")
+GB = input("Enter General Business Score : ")
+ITCS = input("Enter Introduction to Computer Systems Score : ")
+CP = input("Enter Computer Programming Score : ")
+print("--- Your Score ---")
+print("Foundation English : " + FE)
+print("General Business : " + GB)
+print("Introduction to Computer Systems : " + ITCS)
+print("Computer Programming : " + CP)
