e966a9393c9064dcc945e38d736dc0b7da833d05
AntNichapat <146622315+Nichapat05@users.noreply.github.com>
2024-10-27 12:07:29 +0700

    Lec73

---
+++ Lecture73_Nichapat_L.py
@@ -0,0 +1,19 @@
+systemMenu = {"ไก่ทอด": 35, "เป็ดทอด": 45, "ปลาทอด": 55, "ผักทอด": 20}
+menuList = []
+
+def showBill():
+    sum = 0
+    print("---- My Food----")
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+        sum += int(menuList[number][1])
+    print("Total Price : ", sum)
+
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+
+showBill()    
